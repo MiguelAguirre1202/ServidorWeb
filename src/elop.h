ce90edb88c2d0791d67902b4657339a64b41ba80
MiguelAguirre1202 <miguel.aguirre12@gatria.co>
2026-02-26 15:34:56 -0500

    se agrega estilos para el formulario

--- src/elop.h
+++ src/elop.h
@@ -3,9 +3,9 @@
 
 #include <Arduino.h>
 
-extern const uint8_t ELEGANT_HTML[79041];
+extern const uint8_t ELEGANT_HTML[78944];
 
-extern const uint8_t CONFIG_MODEM_HTML[49139];
+extern const uint8_t CONFIG_MODEM_HTML[52490];
 
 
 #endif
