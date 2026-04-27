38bef0179ffcf0f6d92028341708a1d97148d942
MiguelAguirre1202 <miguel.aguirre12@gatria.co>
2026-04-27 10:34:46 -0500

    Refactor input key handling to use text input and implement session storage for configuration key

--- src/elop.h
+++ src/elop.h
@@ -3,9 +3,8 @@
 
 #include <Arduino.h>
 
-extern const uint8_t ELEGANT_HTML[97520];
+extern const uint8_t ELEGANT_HTML[21032];
 
-extern const uint8_t CONFIG_MODEM_HTML[66839];
-
+extern const uint8_t CONFIG_MODEM_HTML[14761];
 
 #endif
