3c0807d27a8d7cdfa508fe1660e565fe5a301ce0
MiguelAguirre1202 <miguel.aguirre12@gatria.co>
2026-05-11 16:32:26 -0500

    Fix METER_DATA size in elop.h to match updated data length

--- src/elop.h
+++ src/elop.h
@@ -7,6 +7,6 @@ extern const uint8_t ELEGANT_HTML[101010];
 
 extern const uint8_t CONFIG_MODEM_HTML[71483];
 
-extern const uint8_t METER_DATA[54774];
+extern const uint8_t METER_DATA[54738];
 
 #endif
