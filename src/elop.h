14d0a9e4e48ab69434b147adb533ea135f1dcf04
MiguelAguirre1202 <miguel.aguirre12@gatria.co>
2026-05-19 11:26:07 -0500

    Refactor code structure for improved readability and maintainability

--- src/elop.h
+++ src/elop.h
@@ -3,8 +3,8 @@
 
 #include <Arduino.h>
 
-extern const uint8_t ELEGANT_HTML[100727];
+extern const uint8_t ELEGANT_HTML[104202];
 
-extern const uint8_t CONFIG_MODEM_HTML[71483];
+extern const uint8_t CONFIG_MODEM_HTML[71956];
 
 #endif
