c895f40922d09d3b2bcc8bc52b176bd907a37703
MiguelAguirre1202 <miguel.aguirre12@gatria.co>
2026-06-25 15:59:35 -0500

    Add: Pages for Naos Modem Lite

--- src/elop.h
+++ src/elop.h
@@ -3,8 +3,8 @@
 
 #include <Arduino.h>
 
-extern const uint8_t ELEGANT_HTML[117417];
+extern const uint8_t ELEGANT_HTML[117391];
 
-extern const uint8_t CONFIG_MODEM_HTML[74729];
+extern const uint8_t CONFIG_MODEM_HTML[74452];
 
 #endif
